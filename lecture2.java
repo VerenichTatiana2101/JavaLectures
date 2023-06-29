47ffbf336ab13bc384e0d0bce2de983b8d3db7cc
Verenich Tatiana <verenik91@gmail.com>
2023-06-30 01:41:45 +0300

    семинар 2

---
+++ lecture2.java
@@ -0,0 +1,153 @@
+// public class lecture2 {
+//     public static void main(String[] args) {
+//     String[] name = { "C", "е", "р", "г", "е", "й" };
+//     String sk = "СЕРГЕЙ КА.";
+//     System.out.println(sk.toLowerCase()); // сергей ка.
+//     System.out.println(String.join("", name)); // Cергей
+//     System.out.println(String.join("", "C", "е", "р", "г", "е", "й"));
+//     // C,е,р,г,е,й
+//     System.out.println(String.join(",", "C", "е", "р", "г", "е", "й"));
+//     }
+//    }
+
+// concat(): объединение строк
+// valueOf(): преобразует Object в строковое представление (завязан на
+// toString())
+// join(): объединяет набор строк в одну с учетом разделителя
+// charAt(): получение символа по индексу
+// indexOf(): первый индекс вхождения подстроки
+// lastIndexOf(): последний индекс вхождения подстроки
+// startsWith()/endsWith(): определяет, начинается/заканчивается ли строка с
+// подстроки
+// replace(): замена одной подстроки на другую
+// trim(): удаляет начальные и конечные пробелы
+// substring(): возвращает подстроку, см.аргументы
+// toLowerCase()/toUpperCase(): возвращает новую строку в нижнем/верхнем
+// регистре
+// сompareTo(): сравнивает две строки
+// equals(): сравнивает строки с учетом регистра
+// equalsIgnoreCase(): сравнивает строки без учета регистра
+// regionMatches(): сравнивает подстроки в строках
+
+// File <имя> = new File(<полный путь к файлу>);
+// File f1 = new File("file.txt");
+// File f2 = new File("/Users/sk/vscode/java_projects/file.txt");
+
+// // Работа с файловой системой. Файлы
+// import java.io.File;
+// public class lecture2 {
+// public static void main(String[] args) {
+// String pathProject = System.getProperty("user.dir");
+// String pathFile = pathProject.concat("/file.txt");
+// File f3 = new File(pathFile);
+// System.out.println(f3.getAbsolutePath ());
+// // /Users/sk/vscode/java_projects/file.txt
+// // C:/Users/Sk/Documents/xxx/brainexplosion/java/file.txt
+// }
+// }
+
+// Работа с файловой системой. Файлы. Ошибки
+// try {
+// Код, в котором может появиться ошибка
+// } catch (Exception e) {
+// Обработка, если ошибка случилась
+// }
+// finally {
+// Код, который выполнится в любом случае
+// }
+
+// import java.io.File;
+
+public class lecture2 {
+    public static void main(String[] args) {
+        try {
+            String pathProject = System.getProperty("user.dir");
+            String pathFile = pathProject.concat("/file.txt");
+            File f3 = new File(pathFile);
+            System.out.println("try");
+        } catch (Exception e) {
+            System.out.println("catch");
+        } finally {
+            System.out.println("finally");
+        }
+    }
+}
+
+// import java.io.File;
+
+// public class lecture2 {
+//     public static void main(String[] args) {
+//         try {
+//             String pathProject = System.getProperty("user.dir");
+//             String pathFile = pathProject.concat("/file.txt");
+//             File file = new File(pathFile);
+//             if (file.createNewFile()) {
+//                 System.out.println("file.created");
+//             } else {
+//                 System.out.println("file.existed");
+//             }
+//         } catch (Exception e) {
+//             System.out.println("catch");
+//         } finally {
+//             System.out.println("finally");
+//         }
+//     }
+// }
+
+// public class lecture2 {
+//     public static void main(String[] args) {
+//         String line = "empty";
+//         try {
+//             File file = new File(pathFile);
+//             if (file.createNewFile()) {
+//                 System.out.println("file.created");
+//             } else {
+//                 BufferedReader bufReader = new BufferedReader(new FileReader(file));
+//                 System.out.println("file.existed");
+//                 line = bufReader.readLine();
+//                 bufReader.close();
+//             }
+//         } catch (Exception e) {
+//             // e.printStackTrace();
+//         } finally {
+//             System.out.println(line);
+//         }
+//     }
+// }   ///не работает
+
+// isHidden(): возвращает истину, если каталог или файл является скрытым
+// length(): возвращает размер файла в байтах
+// lastModified(): возвращает время последнего изменения файла или каталога
+// list(): возвращает массив файлов и подкаталогов, которые находятся в каталоге
+// listFiles(): возвращает массив файлов и подкаталогов, которые находятся
+// в определенном каталоге
+// mkdir(): создает новый каталог
+// renameTo(File dest): переименовывает файл или каталог
+// length(): возвращает размер файла в байтах
+// lastModified(): возвращает время последнего изменения
+// файла или каталога
+// list(): возвращает массив файлов и подкаталогов, которые находятся в каталоге
+// listFiles(): возвращает массив файлов и подкаталогов, которые
+// находятся в определенном каталоге
+// mkdir(): создает новый каталог
+// renameTo(File dest): переименовывает файл или каталог
+
+// import java.io.File;
+
+// public class lecture2 {
+//     public static void main(String[] args) {
+//         String pathProject = System.getProperty("user.dir");
+//         String pathDir = pathProject.concat("/files");
+//         File dir = new File(pathDir);
+//         System.out.println(dir.getAbsolutePath());
+//         if (dir.mkdir()) {
+//             System.out.println("+");
+//         } else {
+//             System.out.println("-");
+//         }
+//         for (String fname : dir.list()) {
+//             System.out.println(fname);
+//         }
+//     }
+// }
+
---
+++ seminar2.java
@@ -0,0 +1,176 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class seminar2 {
+    public static void main(String[] args) throws SecurityException, IOException {  //SecurityException, IOException добавляется при написании кода
+        // System.out.printf(longStringBuilding(10, 'a', 'b'));
+        // System.out.println(stringCompress("aaaabbbcdd"));
+
+        // вызов метода с вычислением времени его работы
+
+        // long startTime = System.nanoTime();
+        // isPalindrome4("aaaabbbaaaa");
+        // long endTime = System.nanoTime();
+        // long duration = (endTime - startTime);
+        // System.out.println("Время выполнения: " + duration + " наносекунд");
+
+        String pathProject = System.getProperty("user.dir"); // определяем наше расположение
+        // String pathFile1 = pathProject.concat("\\Seminar2-1.txt"); // добавляем недостающий \ и имя файла
+        // testTxtFile(pathFile, testString("TEST ", 10)); // куда и что добавили
+
+
+        String pathFile2 = pathProject.concat("\\Seminar2-2.txt");
+        addLogFile(pathFile2, showListFils(pathProject));
+    }
+
+    // Дано четное число N (>0) и символы c1 и c2.
+    // Написать метод, который вернет строку длины N, которая состоит из
+    // чередующихся символов c1 и c2,
+    // начиная с c1.
+    static String longStringBuilding(int n, char c1, char c2) {
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i < n / 2; i++) {
+            newStr.append(c1);
+            newStr.append(c2);
+        }
+        return newStr.toString();
+    }
+
+    // Напишите метод, который сжимает строку.
+    // Пример: вход aaaabbbcdd. Результат - a4b3cd2 для примера.
+    static String stringCompress(String str) {
+        StringBuilder newStr = new StringBuilder();
+        char symbol = str.charAt(0);
+        newStr.append(symbol);
+        int count = 0;
+
+        for (char letter : str.toCharArray()) {
+            if (letter == symbol)
+                count++;
+            else {
+                if (count != 1)
+                    newStr.append(count);
+                newStr.append(letter);
+                symbol = letter;
+                count = 1;
+            }
+        }
+        if (count != 1)
+            newStr.append(count);
+
+        return newStr.toString();
+    }
+
+    // Напишите метод, который принимает на вход строку (String)
+    // и определяет является ли строка палиндромом (возвращает boolean значение).
+    // вариант 1 Время выполнения: 66200 наносекунд
+    static boolean isPalindrome1(String str) {
+        StringBuilder reversedString = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char temp = str.charAt(i);
+            reversedString.append(temp);
+        }
+        return str.equals(reversedString.toString());
+    }
+
+    // вариант 2 Время выполнения: 31300 наносекунд
+    static boolean isPalindrome2(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    // вариант 3 Время выполнения: 69700 наносекунд
+    static boolean isPalindrome3(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        String reversedString = sb.toString();
+        return str.equals(reversedString);
+    }
+
+    // вариант 4 Время выполнения: 10500 наносекунд
+    static boolean isPalindrome4(String word) {
+        int length = word.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (word.charAt(i) != word.charAt(length - i - 1))
+                return false;
+        }
+        return true;
+    }
+
+    // Напишите метод, который составит строку,
+    // состоящую из 100 повторений слова TEST и метод,
+    // который запишет эту строку в простой текстовый файл,
+    // обработайте исключения.
+
+    // метод, который составит строку,
+    // состоящую из 100 повторений слова TEST
+    static String testString(String word, int countWords) {
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i <= countWords; i++) {
+            newStr.append(word);
+        }
+        return newStr.toString() + "\n"; // каждое добавление будет с новой строки
+    }
+
+    // метод, который запишет эту строку в простой текстовый файл
+    static void testTxtFile(String filePath, String testStrTxt) {
+        try {
+            File newFile = new File(filePath);
+            // FileWriter writer = new FileWriter(newFile); //при повторе файл перезапишется
+            FileWriter writer = new FileWriter(newFile, true); // включается функция дозаписи
+
+            writer.write(testStrTxt);
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Напишите метод, который вернет содержимое текущей папки в виде массива строк.
+    // Напишите метод, который запишет массив, возвращенный предыдущим методом в файл.
+    // Обработайте ошибки с помощью try-catch конструкции.
+    // В случае возникновения исключения, оно должно записаться в лог-файл.
+
+    //метод, который вернет содержимое текущей папки в виде массива строк.
+    static String[] showListFils(String dirPath) throws IOException {
+        File currentDir = new File(dirPath);
+        return currentDir.list(); 
+    }
+
+    //метод, который запишет массив, возвращенный предыдущим методом в файл.
+    static void addLogFile(String filePath, String[] list) throws SecurityException, IOException{ 
+        Logger logger = Logger.getLogger(seminar2.class.getName());
+        FileHandler fh = new FileHandler("seminar2-3(log).txt");
+
+        logger.addHandler(fh);   //что куда сохр
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+
+        //отображение логов при возникновении исключений
+        try {
+            File myFile = new File(filePath);
+            FileWriter writer = new FileWriter(myFile); 
+            for (String str : list) {
+                writer.write(str + "\n");
+            }
+            writer.close();
+
+        } catch (Exception e) {
+            logger.warning(e.getMessage());
+        }
+    }
+
+
+}
+
